320d94b561ebeb041d3f8c3ddf9c742da51d8887
01007467319 <57614932+01007467319@users.noreply.github.com>
2021-11-12 22:14:53 +0800

    powsidechain.m

---
+++ powsidechain.m
@@ -0,0 +1,15 @@
+function y=ni(lambda)
+y=[];
+y(1)=0;
+%lambda = x;
+%n = 150000;  %the length of a blockchain; 
+c = 0.5;
+B = 80; %the size of a block header;
+H = 32;       % the size of a hash;
+   for i=1:1:24%20000:600000
+   n = 25000 * (i);
+   % compute equation: log2(2)/log2(1/c)*lambda*((log2(n)+1)*B+log2(n)*(log2(log2(n)))*H)
+   y(i+1) = (lambda*((log2(n)+1)*B+log2(n)*(log2(log2(n)))*H))/1024;
+   %i++
+   end
+end
